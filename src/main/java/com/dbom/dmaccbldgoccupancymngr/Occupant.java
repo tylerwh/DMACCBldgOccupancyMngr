56a3e7a74330f7d6d410f521df74f3f55122c1f9
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2021-12-09 16:24:22 -0600

    Add Building List Add building list of occupants and additional preloaded occupant data for the Spring Boot api

--- src/main/java/com/dbom/dmaccbldgoccupancymngr/Occupant.java
+++ src/main/java/com/dbom/dmaccbldgoccupancymngr/Occupant.java
@@ -46,6 +46,7 @@ public class Occupant {
                 "id=" + id +
                 ", fname='" + fname + '\'' +
                 ", lname='" + lname + '\'' +
+                ", bldgNum=" + bldgNum +
                 '}';
     }
 }
